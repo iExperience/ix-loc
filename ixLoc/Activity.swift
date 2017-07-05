3f3bd7da029f52568cf4e8df1a2c32c2f1aff881
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-07-05 15:31:04 +0200

    Added Latitude and longitude to Activity

--- ixLoc/Activity.swift
+++ ixLoc/Activity.swift
@@ -12,10 +12,19 @@ class Activity {
     
     var name: String?
     var description: String?
+    var latitude: Double?
+    var longitude: Double?
     
     init(name: String?, description: String?) {
         self.name = name
         self.description = description
+    }
+    
+    init(name: String?, description: String?, latitude: Double?, longitude: Double) {
+        self.name = name
+        self.description = description
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
 }
