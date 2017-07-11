5ccc56979e031c423bfe5aa56ef52b95d36062cb
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-07-11 16:17:54 +0200

    Login with Firebase

--- ixLoc/LoginViewController.swift
+++ ixLoc/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FacebookCore
 import FacebookLogin
+import Firebase
 
 class LoginViewController: UIViewController, LoginButtonDelegate {
 
@@ -17,10 +18,12 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*
         if let accessToken = AccessToken.current {
             // User is logged in, use 'accessToken' here.
             self.performSegue(withIdentifier: "navToMain", sender: nil)
         }
+        */
 
         let loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
         //loginButton.center = loginContainerView.center
@@ -30,8 +33,24 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
     }
 
     func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
-        // Navigate to the main view controller
-        self.performSegue(withIdentifier: "navToMain", sender: nil)
+        
+        let credential = FacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+        
+        Auth.auth().signIn(with: credential, completion: {(user, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            
+            if let currentUser = Auth.auth().currentUser {
+                print(currentUser.uid)
+                print(currentUser.displayName)
+                print(currentUser.photoURL)
+            }
+            
+            // Navigate to the main view controller
+            self.performSegue(withIdentifier: "navToMain", sender: nil)
+        })
     }
     
     func loginButtonDidLogOut(_ loginButton: LoginButton) {
